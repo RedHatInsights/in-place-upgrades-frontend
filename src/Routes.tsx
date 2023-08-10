d83fd7d5fe4bea6954dbe0c825c1a969c0492ee9
Andrea Waltlova <awaltlov@redhat.com>
2023-08-10 13:51:34 +0200

    Initialize tab structure and routes
    include test
    
    Signed-off-by: Andrea Waltlova <awaltlov@redhat.com>

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,18 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 
 import { Bullseye, Spinner } from '@patternfly/react-core';
 
-const SamplePage = lazy(() => import(/* webpackChunkName: "SamplePage" */ './Routes/SamplePage/SamplePage'));
-// const OopsPage = lazy(() => import(/* webpackChunkName: "OopsPage" */ './Routes/OopsPage/OopsPage'));
-// const NoPermissionsPage = lazy(() => import(/* webpackChunkName: "NoPermissionsPage" */ './Routes/NoPermissionsPage/NoPermissionsPage'));
+const LandingPage = lazy(() => import('./Components/LandingPage/LandingPage'));
 
-/**
- * the Switch component changes routes depending on the path.
- *
- * Route properties:
- *      exact - path must match exactly,
- *      path - https://prod.foo.redhat.com:1337/insights/advisor/rules
- *      component - component to be rendered when a route has been chosen.
- */
 const CustomRoutes = () => (
   <Suspense
     fallback={
@@ -24,16 +14,7 @@ const CustomRoutes = () => (
     }
   >
     <Routes>
-      <Route path="/" element={<SamplePage />} />
-
-      {/* NOTE: Use below routes for reference for 500 and 403 */}
-      {/* <Route path="/" element={<OopsPage />} /> */}
-      {/* <Route path="/" element={<NoPermissionsPage />} /> */}
-
-      {/* NOTE: Finally, catch all unmatched routes */}
-      {/* <Route>
-        <Redirect to="/" />
-      </Route> */}
+      <Route path="*" element={<LandingPage />} />
     </Routes>
   </Suspense>
 );
